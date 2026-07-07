b497a43e8cc2cbdf95de2873fc167613c9abe2bf
Sneha Yadav <sy8506162@gmail.com>
2026-07-07 16:33:46 +0530

    Added database connection using ADO.NET

---
+++ Models/EmployeeDAL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace EmployeeSystem.Models
+{
+    public class EmployeeDAL
+    {
+
+        string cs =
+ConfigurationManager.ConnectionStrings["EmployeeDB"].ConnectionString;
+    }
+}
